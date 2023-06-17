e0cff4a41bb255c23c538fb6df266e028b61899b
hanbintan2003 <thb182003@gmail.com>
2023-06-17 11:02:26 -0700

    final

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ import { Configuration, OpenAIApi } from 'openai'
 
 dotenv.config()
 
+//console.log(process.env.OPENAI_API_KEY)
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -17,7 +19,7 @@ app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from CodeX!'
+    message: 'Hello from Code Bot!'
   })
 })
 
